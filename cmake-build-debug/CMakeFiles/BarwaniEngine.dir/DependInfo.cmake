61bc461a88217cd38efdbc14554f0466d0d15ad3
Asaad Al-Barwani <asaad.barwani@gmail.com>
2023-09-05 17:49:24 -0400

    sync

---
+++ cmake-build-debug/CMakeFiles/BarwaniEngine.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/asood/CLionProjects/BarwaniEngine/src/AssetManager.cpp" "CMakeFiles/BarwaniEngine.dir/src/AssetManager.cpp.obj" "gcc" "CMakeFiles/BarwaniEngine.dir/src/AssetManager.cpp.obj.d"
+  "C:/Users/asood/CLionProjects/BarwaniEngine/src/Collision.cpp" "CMakeFiles/BarwaniEngine.dir/src/Collision.cpp.obj" "gcc" "CMakeFiles/BarwaniEngine.dir/src/Collision.cpp.obj.d"
+  "C:/Users/asood/CLionProjects/BarwaniEngine/src/CollisionComponent.cpp" "CMakeFiles/BarwaniEngine.dir/src/CollisionComponent.cpp.obj" "gcc" "CMakeFiles/BarwaniEngine.dir/src/CollisionComponent.cpp.obj.d"
+  "C:/Users/asood/CLionProjects/BarwaniEngine/src/ECS.cpp" "CMakeFiles/BarwaniEngine.dir/src/ECS.cpp.obj" "gcc" "CMakeFiles/BarwaniEngine.dir/src/ECS.cpp.obj.d"
+  "C:/Users/asood/CLionProjects/BarwaniEngine/src/Game.cpp" "CMakeFiles/BarwaniEngine.dir/src/Game.cpp.obj" "gcc" "CMakeFiles/BarwaniEngine.dir/src/Game.cpp.obj.d"
+  "C:/Users/asood/CLionProjects/BarwaniEngine/src/KeyboardController.cpp" "CMakeFiles/BarwaniEngine.dir/src/KeyboardController.cpp.obj" "gcc" "CMakeFiles/BarwaniEngine.dir/src/KeyboardController.cpp.obj.d"
+  "C:/Users/asood/CLionProjects/BarwaniEngine/src/SpriteComponent.cpp" "CMakeFiles/BarwaniEngine.dir/src/SpriteComponent.cpp.obj" "gcc" "CMakeFiles/BarwaniEngine.dir/src/SpriteComponent.cpp.obj.d"
+  "C:/Users/asood/CLionProjects/BarwaniEngine/src/TextureManager.cpp" "CMakeFiles/BarwaniEngine.dir/src/TextureManager.cpp.obj" "gcc" "CMakeFiles/BarwaniEngine.dir/src/TextureManager.cpp.obj.d"
+  "C:/Users/asood/CLionProjects/BarwaniEngine/src/TransformComponent.cpp" "CMakeFiles/BarwaniEngine.dir/src/TransformComponent.cpp.obj" "gcc" "CMakeFiles/BarwaniEngine.dir/src/TransformComponent.cpp.obj.d"
+  "C:/Users/asood/CLionProjects/BarwaniEngine/src/Vector2.cpp" "CMakeFiles/BarwaniEngine.dir/src/Vector2.cpp.obj" "gcc" "CMakeFiles/BarwaniEngine.dir/src/Vector2.cpp.obj.d"
+  "C:/Users/asood/CLionProjects/BarwaniEngine/src/main.cpp" "CMakeFiles/BarwaniEngine.dir/src/main.cpp.obj" "gcc" "CMakeFiles/BarwaniEngine.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BarwaniEngine.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "BarwaniEngine.exe"
+  "BarwaniEngine.exe.manifest"
+  "BarwaniEngine.pdb"
+  "CMakeFiles/BarwaniEngine.dir/src/AssetManager.cpp.obj"
+  "CMakeFiles/BarwaniEngine.dir/src/AssetManager.cpp.obj.d"
+  "CMakeFiles/BarwaniEngine.dir/src/Collision.cpp.obj"
+  "CMakeFiles/BarwaniEngine.dir/src/Collision.cpp.obj.d"
+  "CMakeFiles/BarwaniEngine.dir/src/CollisionComponent.cpp.obj"
+  "CMakeFiles/BarwaniEngine.dir/src/CollisionComponent.cpp.obj.d"
+  "CMakeFiles/BarwaniEngine.dir/src/ECS.cpp.obj"
+  "CMakeFiles/BarwaniEngine.dir/src/ECS.cpp.obj.d"
+  "CMakeFiles/BarwaniEngine.dir/src/Game.cpp.obj"
+  "CMakeFiles/BarwaniEngine.dir/src/Game.cpp.obj.d"
+  "CMakeFiles/BarwaniEngine.dir/src/KeyboardController.cpp.obj"
+  "CMakeFiles/BarwaniEngine.dir/src/KeyboardController.cpp.obj.d"
+  "CMakeFiles/BarwaniEngine.dir/src/SpriteComponent.cpp.obj"
+  "CMakeFiles/BarwaniEngine.dir/src/SpriteComponent.cpp.obj.d"
+  "CMakeFiles/BarwaniEngine.dir/src/TextureManager.cpp.obj"
+  "CMakeFiles/BarwaniEngine.dir/src/TextureManager.cpp.obj.d"
+  "CMakeFiles/BarwaniEngine.dir/src/TransformComponent.cpp.obj"
+  "CMakeFiles/BarwaniEngine.dir/src/TransformComponent.cpp.obj.d"
+  "CMakeFiles/BarwaniEngine.dir/src/Vector2.cpp.obj"
+  "CMakeFiles/BarwaniEngine.dir/src/Vector2.cpp.obj.d"
+  "CMakeFiles/BarwaniEngine.dir/src/main.cpp.obj"
+  "CMakeFiles/BarwaniEngine.dir/src/main.cpp.obj.d"
+  "libBarwaniEngine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BarwaniEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
